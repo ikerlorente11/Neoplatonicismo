63f71e609875afd3fb6cf0140ebb1a35133566ba
iker <ikerlorentecalvo@gmail.com>
2021-02-01 13:18:10 +0100

    Creado estructura

---
+++ NeoplatonicismoLib/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoplatonicismoLib
+{
+    public class Contact
+    {
+        String name;
+        String phone;
+        String email;
+
+        public Contact (String Name, String Phone = null, String Email = null)
+        {
+            name = Name;
+            phone = Phone;
+            email = Email;
+        }
+    }
+}
