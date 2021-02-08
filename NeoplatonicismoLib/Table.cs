5c58ce61eccbaca37a652620d93abff3447a5117
iker <ikerlorentecalvo@gmail.com>
2021-02-08 13:23:22 +0100

    Creada estructura

--- NeoplatonicismoLib/Table.cs
+++ NeoplatonicismoLib/Table.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace NeoplatonicismoLib
 {
-    class Table
+    public class Table
     {
+
+        String name;
+        List<TableColumn> columnsType;
+        List<List<String>> rows;
+
+        public Table(String name, List<TableColumn> ColumnsType)
+        {
+            columnsType = ColumnsType;
+        }
+
+        public void AddRow(List<String> row)
+        {
+
+        }
+
+        public void DeleteRow(String columnName, String value)
+        {
+
+        }
+
+        public void Update(String columnName, String value, List<String> row)
+        {
+
+        }
+
+        public void Find(String columnName, String value)
+        {
+
+        }
     }
 }
