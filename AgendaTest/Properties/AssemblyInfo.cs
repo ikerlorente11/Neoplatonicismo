555251ff7bdf53f41e3aa5513a3616ea2e185a80
iker <ikerlorentecalvo@gmail.com>
2021-02-08 12:28:56 +0100

    Creada NeoplatonicismoLib  yrenombrada Agenda

--- AgendaTest/Properties/AssemblyInfo.cs
+++ AgendaTest/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NeoplatonicismoTest")]
+[assembly: AssemblyTitle("AgendaTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NeoplatonicismoTest")]
+[assembly: AssemblyProduct("AgendaTest")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("15edc519-c4d7-494f-b3ef-2b8fb7dc6fb6")]
+[assembly: Guid("678ec041-d41b-408e-9719-2468992f5520")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
