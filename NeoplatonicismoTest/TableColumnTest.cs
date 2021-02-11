937013c0af01228ed3a62ebeca38b69d0a69c216
Alex <ass0100.csmm@gmail.com>
2021-02-11 11:52:27 +0100

    TableColumnTest structure created

--- NeoplatonicismoTest/TableColumnTest.cs
+++ NeoplatonicismoTest/TableColumnTest.cs
@@ -1,13 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NeoplatonicismoTest;
 
 namespace NeoplatonicismoTest
 {
+    [TestClass]
     class TableColumnTest
     {
+        [TestMethod]
+        public void GetNameTest()
+        {
+
+        }
+        [TestMethod]
+        public void GetTypeValueTest()
+        {
+        
+        }
+
     }
 }
