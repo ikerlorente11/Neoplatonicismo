8de14bd693e70f0746f336f649773e8024d919b3
Andrew Díez <andresdiezmtz@gmail.com>
2021-02-15 12:23:12 +0100

    initialitation tables, getTables, create tables, findTable

--- NeoplatonicismoLib/Database.cs
+++ NeoplatonicismoLib/Database.cs
@@ -11,7 +11,7 @@ namespace NeoplatonicismoLib
         String name;
         String username;
         String password;
-        List<Table> tables;
+        List<Table> tables= new List<Table>();
 
         public Database(String Name, String Username, String Password)
         {
@@ -27,12 +27,13 @@ namespace NeoplatonicismoLib
 
         public List<Table> GetTables()
         {
-            return null;
+            return tables;
         }
 
         public void CreateTable(String tableName, List<TableColumn> tableColumns )
         {
-
+            Table table = new Table(tableName, tableColumns);
+            tables.Add(table);
         }
 
         public void DropTable(String tableName)
@@ -45,8 +46,13 @@ namespace NeoplatonicismoLib
 
         }
 
-        public List<Table> FindTable(String tableName)
+        public Table FindTable(String tableName)
         {
+            foreach(Table table in tables)
+            {
+                if (table.GetName() == tableName)
+                    return table;
+            }
             return null;
         }
 
