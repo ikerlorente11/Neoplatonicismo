3a5a800b3228044b4552c4f4c93d5e7bc9c63933
iker <ikerlorentecalvo@gmail.com>
2021-03-08 12:43:14 +0100

    #15 SelectColumns parser implemented

--- NeoplatonicismoTest/ParserTest.cs
+++ NeoplatonicismoTest/ParserTest.cs
@@ -14,6 +14,15 @@ namespace NeoplatonicismoTest
             IQuery query = Parser.Parse("SELECT * FROM people;");
             Assert.IsTrue(query is SelectAll);
             Assert.AreEqual("people", (query as SelectAll).Table());
+
+            query = Parser.Parse("SELECT nombre FROM people WHERE nombre = pepe;");
+            Assert.IsTrue(query is SelectColumns);
+            Assert.AreEqual("people", (query as SelectColumns).Table());
+            Assert.AreEqual("nombre", (query as SelectColumns).ColumnNames()[0]);
+            Assert.AreEqual("nombre", (query as SelectColumns).ColumnName());
+            String pepe = (query as SelectColumns).ColumnValue();
+            Assert.AreEqual("pepe", (query as SelectColumns).ColumnValue());
+            Assert.AreEqual('=', (query as SelectColumns).Operation());
         }
     }
 }
