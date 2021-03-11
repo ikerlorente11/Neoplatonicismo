cab0ea1b4892dff6f4d9023d8cc62f80d9c070b3
maria <maria@DESKTOP-0HJT21N>
2021-03-11 13:25:53 +0100

    #18 run delete

--- NeoplatonicismoLib/MiniSQLQuery/Delete.cs
+++ NeoplatonicismoLib/MiniSQLQuery/Delete.cs
@@ -43,8 +43,20 @@ namespace NeoplatonicismoLib.MiniSQLQuery
 
             public string Run(Database database)
             {
+            if (m_column == null)
+            {
+                database.DropTable(m_table);
 
-                return "";
+            }
+            else
+            {
+                List<Table> tables = database.GetTables();
+                Table table = tables[database.FindTable(m_table)];
+                table.DeleteRow(m_column, m_value, m_operation);
+
+            }
+
+                return "Tupla(s) deleted.";
             }
     }
 
