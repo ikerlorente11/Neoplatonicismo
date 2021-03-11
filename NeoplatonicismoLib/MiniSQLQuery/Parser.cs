9f1204667534f2434c534e9d94af75d32668ec0e
maria <maria@DESKTOP-0HJT21N>
2021-03-11 12:32:24 +0100

    #18 delete y merges

--- NeoplatonicismoLib/MiniSQLQuery/Parser.cs
+++ NeoplatonicismoLib/MiniSQLQuery/Parser.cs
@@ -14,8 +14,6 @@ namespace NeoplatonicismoLib.MiniSQLQuery
             const string selectAllParameter = @"SELECT \* FROM ([a-zA-Z0-9]+)";
             const string selectColumnsPattern = @"SELECT ([a-zA-Z0-9,]+) FROM ([a-zA-Z0-9]+)";
             const string delete = @"DELETE FROM ([A-Za-z0-9]+_[A-Zaa-z0-9]+) WHERE ([A-Za-z0-9]+) ([<->-=]) ([A-Za-z0-9].+)";
-            const string selectAllParameter = @"SELECT \* FROM ([a-zA-Z0-9]+)";
-            const string selectColumnsPattern = @"SELECT ([a-zA-Z0-9,]+) FROM ([a-zA-Z0-9]+)";
             const string insert = @"INSERT INTO ([A-za-z0-9]+) VALUES \((([A-za-z0-9]+),?)+\)";
 
             Match match = Regex.Match(miniSqlSencence, selectAllParameter);
